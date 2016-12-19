77b5ce9654e44bc632aed0896755436994419279
Dayne Batten <menuet@gmail.com>
2016-12-19 13:41:20 -0500

    Bool, float, regex, string checks.

---
+++ columncheck/com/rw/legion/columncheck/FloatChecker.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2016 Republic Wireless
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rw.legion.columncheck;
+
+import com.google.gson.Gson;
+
+public class FloatChecker {
+    private String floatType;
+    private Properties props;
+    private Gson gson;
+    
+    public FloatChecker() {
+        gson = new Gson();
+    }
+    
+    public boolean validates(String str) {
+        try {
+            if (floatType.equals("float")) Float.parseFloat(str);
+            if (floatType.equals("double")) Double.parseDouble(str);
+            
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+    }
+    
+    public void fromJson(String json) {
+        props = gson.fromJson(json, Properties.class);
+        
+        floatType = props.getFloatType();
+    }
+    
+    public String toJson() {
+        return gson.toJson(props);
+    }
+    
+    static class Properties {
+        String floatType;
+        
+        public String getFloatType() {
+            return floatType;
+        }
+    }
+}
---
+++ columncheck/com/rw/legion/columncheck/StringChecker.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016 Republic Wireless
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rw.legion.columncheck;
+
+public class StringChecker {
+    public boolean validates(String str) {
+        return true;
+    }
+    
+    public void fromJson(String json) {
+        // Nothing to do here.
+    }
+    
+    public String toJson() {
+        return "";
+    }
+    
+    static class Properties {
+        // No properties...
+    }
+}
